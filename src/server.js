f34507d538231473074caa94021089f2572cf60d
zomg830 <matt.faubel@gmail.com>
2019-03-24 20:49:21 -0500

    Heroku initializing

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const hbs = require("hbs");
 const friends = require("../public/js/friends");
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
